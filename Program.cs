53d17074327b1c64361121d9b7ac9cfa61d0d35e
Nick <74838258+xadmin20@users.noreply.github.com>
2022-09-01 13:43:51 +0300

    Задача №29

--- Program.cs
+++ Program.cs
@@ -1,19 +1,27 @@
-﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-// 452 -> 11
-// 82 -> 10
-// 9012 -> 12
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
 
-void sqr()
+Random rd = new Random();
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] arr = FillArray(N);
+int[] FillArray(int number)
 {
-    Console.Write("Введите число: ");
-    int num = Convert.ToInt32(Console.ReadLine());
-    int sum = 0;
-    while(num!=0)
-    {
-        sum = sum + num % 10;
-        num = num / 10;
-    }
-    Console.WriteLine($"Сумма чисел равна: {sum}");
-    }
+    int[] array = new int[number];
+    for (int i = 0; i < N; i++) array[i] = rd.Next(1, 100);
+    return array;
+}
 
-sqr();
+void printArray(int[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (i==0) Console.Write($"[{Array[i]}, ");
+        else if (i>0 && i!=N-1) Console.Write($"{Array[i]}, ");
+        else Console.WriteLine($"{Array[i]}]");
+    }
+    Console.Write("");
+}
+
+printArray(arr);
